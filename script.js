4a3eb726fc78e1ad18a77cc4a0958e007c71e4d1
Va Muradyan <P0stNe0n@yandex.ru>
2020-08-11 12:55:13 +0300

    работа на ошибками

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+// По поводу гит хаба не понимаю в чем проблема , стили не подгружаются.
 let profile = document.querySelector(".profile");
 let popup = document.querySelector(".popup");
 let editButton = profile.querySelector(".profile__editButton");
 let closePopup = popup.querySelector(".popup__close");
 let saveButton = popup.querySelector(".popup__button");
 let userForm = document.querySelector(".popup__form");
+let nameInput = document.querySelector(".popup__input_type_name");
+let jobInput = document.querySelector(".popup__input_type_proff");
+let userName = document.querySelector(".profile__info-title");
+let userProff = document.querySelector(".profile__info-subtitle");
 
 function exitPopup() {
   popup.style.display = "none";
@@ -11,35 +16,20 @@ function exitPopup() {
 
 function openPopup() {
   popup.style.display = "block";
+  nameInput.value = userName.textContent; 
+  jobInput.value = userProff.textContent;
 }
 
-editButton.addEventListener("click", openPopup);
-closePopup.addEventListener("click", exitPopup);
-
 function formSubmitHandler(evt) {
-  // Эта строчка отменяет стандартную отправку формы.
-  // Так мы можем определить свою логику отправки.
-  // О том, как это делать, расскажем позже.
-
-  // Находим поля формы в DOM
-  let nameInput = document.querySelector(".popup__input_type_name");
-  let jobInput = document.querySelector(".popup__input_type_proff");
-
-  nameInput = nameInput.value
-  jobInput = jobInput.value
-  // Получите значение полей из свойства value
-
-  let userName = document.querySelector(".profile__info-title");
-  let userProff = document.querySelector(".profile__info-subtitle");
-   // Выберите элементы, куда должны быть вставлены значения полей
-
+  nameInput=nameInput.value;
+  jobInput=jobInput.value;
   userName.textContent = nameInput;
   userProff.textContent = jobInput;
 
   exitPopup();
-  evt.preventDefault(); // Вставьте новые значения с помощью textContent
+  evt.preventDefault();
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 userForm.addEventListener("submit", formSubmitHandler);
+editButton.addEventListener("click", openPopup);
+closePopup.addEventListener("click", exitPopup);
